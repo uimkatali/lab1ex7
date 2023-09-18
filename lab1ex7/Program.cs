4c81694ae9e515f71a85fe86416c49819a05fbe2
uimkatali <b8bf6b5@gmail.com>
2023-09-18 13:23:51 +0300

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab1ex7
+{
+
+    class Program
+
+    /*Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+    impar
+    */
+    {
+        static void Main(string[] args)
+        {
+            int numar = Convert.ToInt32(Console.ReadLine());
+            if(numar % 2 == 0 ) {
+                Console.WriteLine("Numar par!");
+            }
+            else { Console.WriteLine("Numar impar"); }
+        }
+    }
+}
